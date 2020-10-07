20a2adc102d37fccb24c7d19f3384870b1a75df7
Luisandra <LuisandraMM@gmail.com>
2020-10-07 18:06:36 -0500

    initial commit

---
+++ Medium/sum_nodes_w_even_grandprnt.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const a = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5] //output 18
+var sumEvenGrandparent = function(root) {
+
+};
