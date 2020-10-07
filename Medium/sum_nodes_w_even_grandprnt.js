08dd0288947be59ad6f07b79a478e834fddd2872
Luisandra <LuisandraMM@gmail.com>
2020-10-07 18:15:30 -0500

    Solution - don't understand

--- Medium/sum_nodes_w_even_grandprnt.js
+++ Medium/sum_nodes_w_even_grandprnt.js
@@ -12,5 +12,22 @@
  */
 const a = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5] //output 18
 var sumEvenGrandparent = function(root) {
-
+//DO NOT UNDERSTAND HOW THIS WORKS
+    return dfs(root, []);
 };
+
+function dfs (root, sequence) {
+    if (!root) {
+        return 0;
+    }
+    let result = 0;
+    if (sequence.length > 1 && sequence[sequence.length - 2] % 2 === 0) {
+        result += root.val;
+    }
+    sequence.push(root.val);
+    result += dfs(root.left, sequence);
+    result += dfs(root.right, sequence);
+    sequence.pop();
+    return result;
+};
+sumEvenGrandparent(a)
