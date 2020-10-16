160f7b48f1c6f721e919b5a422617e2db0bdc908
Luisandra <LuisandraMM@gmail.com>
2020-10-15 20:04:26 -0500

    solved in ONE LINE!

---
+++ Batch3/Easy/peak_idx_of_mountain.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+const a=[0,1,0], b=[0,10,5,2], c=[24,69,100,99,79,78,67,36,26,19]
+var peakIndexInMountainArray = function(arr) {
+    // for(let i=1; i<arr.length; i++){
+    //     if(arr[i]<arr[i-1]){
+    //         return (i-1)
+    //     }
+    // }
+//got it down to one line of code!!!!!
+    console.log(arr.indexOf(Math.max(...arr)))
+};
+peakIndexInMountainArray(c)
