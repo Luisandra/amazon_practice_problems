d785c69c7e11b4cab279d75aa1c404b6d896bd91
Luisandra <LuisandraMM@gmail.com>
2020-10-24 17:18:27 -0500

    do not understand, no solution

---
+++ Batch3/Medium/delete_node_rtn_forest.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number[]} to_delete
+ * @return {TreeNode[]}
+ */
+const a = [1,2,3,4,5,6,7], to_delete = [3,5] //output [[1,2,null,4],[6],[7]]
+var delNodes = function(root, to_delete) {
+
+};
+delNodes(a)
